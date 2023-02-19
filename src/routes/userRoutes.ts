f1d886d6e15eedcd94d51f177bdd88f03ff0cb05
SRenuuu <sandulrenuja@gmail.com>
2023-02-20 00:36:40 +0530

    implement get user and refactor code

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,7 +2,7 @@ import express from "express";
 import {getUser, updateUser, uploadAvatar} from "../controllers/userController";
 const router = express.Router();
 
-router.route("/:id").get(getUser).put(updateUser);
+router.route("/").get(getUser).put(updateUser);
 router.route("/upload-avatar").post(uploadAvatar);
 
 export default router;
