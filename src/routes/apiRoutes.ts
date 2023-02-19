3c5a45a28b0ac4e91209b549e291e7dc423f9b1a
Nipun Chamikara <nipunchamikara@yahoo.com>
2023-02-19 21:52:24 +0530

    add user and category routes to apiRoutes.ts

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import authRoutes from "./authRoutes";
 import userRoutes from "./userRoutes";
+import todoRoutes from "./todoRoutes";
 import {verifyAuthToken} from "../middleware/authTokenChecker";
+import categoryRoutes from "./categoryRoutes";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
-router.use("/user", verifyAuthToken, userRoutes);
+router.use("/users", verifyAuthToken, userRoutes);
+router.use("/todos", verifyAuthToken, todoRoutes);
+router.use("/categories", categoryRoutes);
 
 export default router;
