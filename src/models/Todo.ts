301120e64339f443206558ec84bc0e16dc8b236e
Nipun Chamikara <nipunchamikara@yahoo.com>
2023-02-20 18:58:27 +0530

    Refactor code Change response structure

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,11 +1,11 @@
 export default interface Todo {
-    todo_id: string;
-    title: string;
-    notes: string;
-    created_dt: string;
-    due_dt: string;
-    is_completed: string;
-    last_modified_dt: string;
-    user_id: string;
-    category_id: string;
+  todo_id: string;
+  title: string;
+  notes: string;
+  created_dt: string;
+  due_dt: string;
+  is_complete: string;
+  last_modified_dt: string;
+  user_id: string;
+  category_id: string;
 }
